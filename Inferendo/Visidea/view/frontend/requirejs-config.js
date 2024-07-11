7224cd1001bc54fce2209712a5f09abc918f3fdb
Alessandro Rolando <ale@inferendo.com>
2024-07-11 09:03:30 +0200

    semantic search new plugin

--- Inferendo/Visidea/view/frontend/requirejs-config.js
+++ Inferendo/Visidea/view/frontend/requirejs-config.js
@@ -1,7 +1,5 @@
 var config = {
     paths: {
-        'Visidea': '//cdn.visidea.ai/js-api-client/dist/visidea-api-client.min',
-        'VisideaMagento': 'Inferendo_Visidea/js/visidea-magento',
-        'Slick': '//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min'
+        'Visidea': '//cdn.visidea.ai/js-visidea/js-visidea-3.0.0.min'
     }
 };
